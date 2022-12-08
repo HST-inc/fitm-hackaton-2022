4b4f97cbd7190663f5e800c283a90872069bf7ee
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-12-08 15:58:12 +0300

    Created user's database logic

--- backend/db/users.py
+++ backend/db/users.py
@@ -8,10 +8,16 @@ users = sqlalchemy.Table(
     sqlalchemy.Column("id", sqlalchemy.Integer,
                       autoincrement=True, unique=True, primary_key=True),
     sqlalchemy.Column("first_name", sqlalchemy.String, primary_key=True),
-    sqlalchemy.Column("second_name", sqlalchemy.String, primary_key=True),
-    sqlalchemy.Column("age", sqlalchemy.Integer),
+    sqlalchemy.Column("patronymic_name", sqlalchemy.String, primary_key=True),
+    sqlalchemy.Column("birth_date", sqlalchemy.DateTime, ptimary_key=True),
+    sqlalchemy.Column("last_name", sqlalchemy.String, primary_key=True),
     sqlalchemy.Column("created_time", sqlalchemy.DateTime,
                       default=datetime.datetime.utcnow),
     sqlalchemy.Column("hashed_password", sqlalchemy.String,
                       unique=True, primary_key=True),
+    sqlalchemy.Column("sex", sqlalchemy.String, deault="male"),
+    sqlalchemy.Column("phone_number", sqlalchemy.String),
+    sqlalchemy.Column("snus", sqlalchemy.String),
+    sqlalchemy.Column("passport_series", sqlalchemy.String),
+    sqlalchemy.Column("passport_number", sqlalchemy.String)
 )
