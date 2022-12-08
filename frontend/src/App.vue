b1e38ba3a310b6cbf6c8fa26ea3d62b2c7c265bd
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-12-08 17:48:55 +0300

    Kolya pushit v main

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <HelloWorld v-bind:msg="msg"/>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import HelloWorld from './components/HelloWorld.vue';
+
+  const msg = ref(import.meta.env.VITE_SOME_KEY);
+</script>
+
+<style scoped>
+  .logo {
+      height: 6em;
+      padding: 1.5em;
+      will-change: filter;
+  }
+  .logo:hover {
+      filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+      filter: drop-shadow(0 0 2em #42b883aa);
+  }
+</style>
