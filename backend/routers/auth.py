98420c31bb0a33105c43e471d35da4341b41d4d2
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-12-09 00:17:47 +0300

    Creates sceleton

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -3,6 +3,16 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
+@router.get("/api/v1/sign_in/")
+async def sign_in():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@router.get("/api/v1/sign_up/")
+async def sign_up():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
 @router.get("/api/v1/auth/")
-async def read_auth():
+async def auth():
     return [{"username": "Rick"}, {"username": "Morty"}]
