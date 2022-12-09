75476f5eb4b427d33bc361cfcad23f2b4a3586f4
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-12-09 03:09:11 +0300

    Changed paths

--- backend/packages/routers/user.py
+++ backend/packages/routers/user.py
@@ -6,16 +6,16 @@ from packages.services.user import getpatients, getdoctor, getpatientsshort
 router = APIRouter()
 
 
-@router.get("/api/v1/get_patients/")
+@router.get("/api/v1/user/get_patients/")
 async def get_patients(id: int):
     return getpatients(id)
 
 
-@router.get("/api/v1/get_patients_short/")
+@router.get("/api/v1/user/get_patients_short/")
 async def get_patients_short(id: int):
     return getpatientsshort(id)
 
 
-@router.get("/api/v1/get_patients/")
+@router.get("/api/v1/user/get_patients/")
 async def get_doctor(id: int):
     return getdoctor(id)
