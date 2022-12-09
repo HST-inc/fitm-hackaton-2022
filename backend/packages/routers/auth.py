a5b031bd36076d80d16e9abb01be6cdd765db2f6
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-12-09 03:08:33 +0300

    Changed paths

--- backend/packages/routers/auth.py
+++ backend/packages/routers/auth.py
@@ -6,26 +6,26 @@ from packages.services.auth import signin, signup, log_out
 router = APIRouter()
 
 
-@router.get("/api/v1/sign_in/")
+@router.get("/api/v1/auth/signin/")
 async def sign_in(signin_dto: SignInDto):
     return signin(signin_dto)
 
 
-@router.get("/api/v1/sign_in/")
+@router.get("/api/v1/auth/signin/token/")
 async def sign_in_by_token(token: str):
     return sign_in_by_token(token)
 
 
-@router.get("/api/v1/sign_up/")
+@router.get("/api/v1/auth/signup/")
 async def sign_up(signup_dto: SignUpDto):
     return signup(signup_dto)
 
 
-@router.get("/api/v1/logout/")
+@router.get("/api/v1/auth/logout/")
 async def logout(token: str):
     log_out(token)
 
 
-@router.get("/api/v1/refresh_token")
+@router.get("/api/v1/auth/refresh")
 async def refresh_token(token: str):
     return sign_in_by_token(token)
